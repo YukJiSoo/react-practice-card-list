5ee285c506228ceda6366e5ce88741073938cdc5
YukJiSoo <wltn3231@naver.com>
2020-01-07 19:07:38 +0900

    feat: card list 스타일 구현
    card를 포함하는 card list의 스타일을 구현했습니다.
    css grid를 사용하여 구현했습니다.

---
+++ src/containers/Products/Main/style.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+
+export const Main = styled.main`
+    padding-top: 20vh;
+`;
+
+export const CardList = styled.section`
+    display: grid;
+
+    @media only screen and (min-width: 1024px) {
+        grid-template-columns: repeat(4, 20vw);
+        grid-column-gap: 4vw;
+        grid-row-gap: 4vw;
+        margin: 0 4vw;
+    }
+
+    @media only screen and (max-width: 1024px) {
+        grid-template-columns: repeat(3, 24vw);
+        grid-column-gap: 7vw;
+        grid-row-gap: 7vw;
+        margin: 0 7vw;
+    }
+
+    @media only screen and (max-width: 768px) {
+        grid-template-columns: repeat(2, 41vw);
+        grid-column-gap: 6vw;
+        grid-row-gap: 6vw;
+        margin: 0 6vw;
+    }
+
+    @media only screen and (max-width: 600px) {
+        grid-template-columns: repeat(1, 90vw);
+        grid-row-gap: 5vw;
+        margin: 0 5vw;
+    }
+`;
