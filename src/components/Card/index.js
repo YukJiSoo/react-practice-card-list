6100112d447c2069b52e1925f970eb3b0dd6db3e
YukJiSoo <wltn3231@naver.com>
2020-01-07 21:15:30 +0900

    feat: Card component props 추가
    넘겨받은 props로 rendering하도록 props를 추가해주었습니다.

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -5,18 +5,15 @@ import Wish from 'components/Wish';
 
 const WISH_COMPONENT_SIZE = '1.5rem';
 
-const Card = () => {
+const Card = ({ id, thumbnailPath, name, price }) => {
     return (
         <Styles.Card>
             <Styles.WishWrapper>
                 <Wish isTrue={true} size={WISH_COMPONENT_SIZE} />
             </Styles.WishWrapper>
-            <Styles.Thumbnail src="https://d2ur7st6jjikze.cloudfront.net/share/thumbnail/thumbnail-1.jpg" />
-            <Styles.Name>
-                [호텔] 상트페레트부르크에서 출발하는 푸쉬킨(차르스코예 셀로)과
-                예카테리나
-            </Styles.Name>
-            <Styles.Price>20105원</Styles.Price>
+            <Styles.Thumbnail src={thumbnailPath} />
+            <Styles.Name>{name}</Styles.Name>
+            <Styles.Price>{price}</Styles.Price>
         </Styles.Card>
     );
 };
