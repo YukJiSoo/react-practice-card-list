ef215ab930fb22f393315fdb4044ef5b128125c6
YukJiSoo <wltn3231@naver.com>
2020-01-07 21:25:39 +0900

    feat: 상품데이터 fetch 로직 구현
    Product page에서 data를 fetch 한 후, state를 변경해주는 로직을 작성했습니다.
    useFakeFetch hooks를 이용하여 data를 받아오도록 했습니다.

--- src/pages/Products/index.js
+++ src/pages/Products/index.js
@@ -1,12 +1,41 @@
 import * as React from 'react';
+import { useEffect, useContext } from 'react';
 
-import { Header } from 'containers/Products';
+import { Header, Main } from 'containers/Products';
+
+import { useFakeFetch } from 'hooks';
+
+import ProductsDummyData from 'data/procucts';
+
+import { ProductContext } from 'store/Product';
+import { newProductsFetchedActionCreator } from 'actions/Product';
 
 const Products = () => {
-    console.log(Header);
+    const { dispatchProduct } = useContext(ProductContext);
+    const [{ data }, fetchData] = useFakeFetch();
+
+    const handleFirstFetchData = () => {
+        const data = ProductsDummyData;
+        const startDataOrder = 0;
+        const dataNumber = 10;
+        fetchData({ data, startDataOrder, dataNumber });
+    };
+    const handleDataFetched = () => {
+        if (!data) return;
+        const payload = { newProducts: data };
+        const newProductsFetchedAction = newProductsFetchedActionCreator(
+            payload
+        );
+        dispatchProduct(newProductsFetchedAction);
+    };
+
+    useEffect(handleFirstFetchData, []);
+    useEffect(handleDataFetched, [data]);
+
     return (
         <>
             <Header />
+            <Main />
         </>
     );
 };
