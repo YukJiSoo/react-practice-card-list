023146d3d6840380a17187913a130d13751d9be0
YukJiSoo <wltn3231@naver.com>
2020-01-07 22:46:18 +0900

    feat: image lazy loading 구현
    IntersectionObserver API를 이용하여 lazy loading을 구현했습니다.

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useEffect, useRef } from 'react';
 import * as Styles from './style';
 
 import Wish from 'components/Wish';
@@ -6,12 +7,37 @@ import Wish from 'components/Wish';
 const WISH_COMPONENT_SIZE = '1.5rem';
 
 const Card = ({ id, thumbnailPath, name, price }) => {
+    const imgRef = useRef();
+
+    const handleLazyLoadImage = () => {
+        const options = {
+            root: null,
+            rootMargin: '100px',
+            threshold: 0,
+        };
+        const handleInIntersecting = (observer, entry) => {
+            const { target } = entry;
+            if (!entry.isIntersecting) return;
+            target.src = target.dataset.src;
+            observer.unobserve(target);
+        };
+        const callbackFunction = (entries, observer) => {
+            entries.forEach(handleInIntersecting.bind(undefined, observer));
+        };
+
+        const observer = new IntersectionObserver(callbackFunction, options);
+        observer.observe(imgRef.current);
+
+        return () => observer && observer.disconnect();
+    };
+
+    useEffect(handleLazyLoadImage, []);
     return (
         <Styles.Card>
             <Styles.WishWrapper>
                 <Wish isTrue={true} size={WISH_COMPONENT_SIZE} />
             </Styles.WishWrapper>
-            <Styles.Thumbnail src={thumbnailPath} />
+            <Styles.Thumbnail ref={imgRef} data-src={thumbnailPath} />
             <Styles.Name>{name}</Styles.Name>
             <Styles.Price>{price}</Styles.Price>
         </Styles.Card>
