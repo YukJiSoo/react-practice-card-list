ca7d98cc5af1d27a3263cdb3a647a23d97b7bd74
YukJiSoo <wltn3231@naver.com>
2020-01-08 18:59:30 +0900

    fix: 정렬 option 뒷 배경도 클릭 이벤트에 반응하는 에러 수정
    option목록의 배경에서는 event에 반응하지 않도록 수정했습니다.

--- src/components/OptionList/index.js
+++ src/components/OptionList/index.js
@@ -2,8 +2,12 @@ import * as React from 'react';
 import * as Styles from './style';
 
 const OptionList = ({ optionList, selectedOption = 0, handleSelectOption }) => {
+    const onClick = e => {
+        if (e.currentTarget === e.target) return;
+        handleSelectOption(e);
+    };
     return (
-        <Styles.OptionList onClick={handleSelectOption}>
+        <Styles.OptionList onClick={onClick}>
             {optionList.map((name, index) => (
                 <Styles.Option
                     key={`option-${index}`}
